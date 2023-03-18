5e21287e2254c7567f4eaf624a67a7321e4f7e6e
Benoit Durand <benoit.durand.indep@gmail.com>
2023-03-18 21:32:07 +0100

    add /models /items

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from fastapi import FastAPI
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/me")
+
+@ app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+@ app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "have some residuals"}
+
+
+@ app.get("/items/me")
 async def readm_me():
     return {"item": "my item"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
+@ app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+
+@ app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip+limit]
+
+
+@ app.get("/")
+async def root():
+    return {"message": "Hello World"}
