752dc252561f9526dfd6d63167286ef104af9adb
Benoit Durand <benoit.durand.indep@gmail.com>
2023-03-31 20:13:31 +0200

    finish query param and create path.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,  Query
 from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
 
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 app = FastAPI()
@@ -35,16 +44,71 @@ async def readm_me():
     return {"item": "my item"}
 
 
-@ app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
+@ app.get("/users/{user_id}/items/{item_id}")
+async def read_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "An amazing item !"})
+    return item
+
+
+@ app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "An amazing item !"})
+    return item
+
+
+@app.get("/items_list/")
+async def read_items(q: Annotated[list, Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/items3/")
+# ... to default value if value is required
+async def read_items(q: Annotated[str | None, Query(title="Query string",
+                                                    description="Query string for the items to search in the db",
+                                                    alias="item-query", min_length=3, max_length=50, deprecated=True, include_in_schema=False)] = "fixedquery"):
+
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@ app.get("/items2/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item_id = {"item_id": item_id, "needy": needy,
+               "skip": skip, "limit": limit}
+    return item_id
 
 
 @ app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip:skip+limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price+item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 @ app.get("/")
---
+++ path.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Path,  Query
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_items(item_id: Annotated[int, Path(title="The id of the item to get",ge=1, le=1000)],
+                      size : Annotated[float,Query(gt=0,lt=10.5)],
+                     q: Annotated[str | None, Query(alias="item-query")] = None):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
